c7051c80f0abe05ea47e8c3d36b763b54b1fda4c
herojeff02 <herojeff02@gmail.com>
2022-12-11 22:57:54 +0900

    I'm not proud of this code like for real

--- Backend/websocket.py
+++ Backend/websocket.py
@@ -50,20 +50,52 @@ def init(device_name, websocket):
     else:
         return False
 
-async def start_chirps():
-    if len(students) + len(corners) < 0:
-        return False
+
+async def acquire_lock():
+    # lock
+    global lock
+    if lock == 0:
+        lock = 1
+        return True
     else:
-        # (A => X) chirp 재생
-        # A의 websocket 찾기
-        await tellDeviceToChirp("X", "A")
-        # BeepBeep()
+        return False
 
-        # await tellDeviceToChirp("A", "Y")
-        # await tellDeviceToChirp("A", "Z")
-        
+def release_lock():
+    # lock
+    global lock
+    if lock == 1:
+        lock = 0
+        return True
+    else:
+        return False
 
-async def tellDeviceToChirp(student, corner):
+# 웹소켓 설정 ws://127.0.0.1:8000/ws 로 접속할 수 있음
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    print(f"client connected : {websocket.client}")
+    await websocket.accept() # client의 websocket접속 허용
+    await websocket.send_text(f"Welcome client : {websocket.client}")
+    all_sockets.append(websocket)
+
+    while True:
+        data = await websocket.receive_json()  # client 메시지 수신대기
+        print(f"message received : {data} from : {websocket.client}")
+
+        if data["type"] == "init":
+            init(data["device_name"], websocket)
+        elif data["type"] == "start":
+            if len(students) + len(corners) < 0:
+                return False
+            else:
+                break
+
+                
+        else:
+            print("정의되지 않은 type입니다.")
+
+
+    student = "X"
+    corner = "A"
     first_send = students[student]
     second_send = corners[corner]
 
@@ -72,9 +104,9 @@ async def tellDeviceToChirp(student, corner):
 
     await first_send.send_text(f"빨리 {corner}한테 첩 보내")
     print("trying to receive")
-    data = await first_send.receive_text()
+    data = await first_send.receive_json()
     print(data)
-    # await first_send.receive_text() # 다 했어 받을때까지 기다림 -> 여기서 터짐, 
+    # await first_send.receive_text()
     # cannot call recv while another coroutine is already waiting for the next message
 
     await second_send.send_text(f"빨리 {student}한테 첩 보내")
@@ -103,70 +135,6 @@ async def tellDeviceToChirp(student, corner):
     f.write(s_file)
     f.close()
 
-    return True
-
-async def acquire_lock():
-    # lock
-    global lock
-    if lock == 0:
-        lock = 1
-        return True
-    else:
-        return False
-
-def release_lock():
-    # lock
-    global lock
-    if lock == 1:
-        lock = 0
-        return True
-    else:
-        return False
-
-
-@app.get("/client")
-async def client(request: Request):
-    # /templates/client.html파일을 response함
-    return templates.TemplateResponse("client.html", {"request":request})
-
-@app.post("/files/")
-async def receive_audio_files(files: List[bytes] = File(...), sender_device_name = "None", receiver_device_name = "None"):
-    f = open(f'uploaded_files/{sender_device_name}_{receiver_device_name}.wav', 'w')
-    f.write(files)
-    f.close()
-    return True
-
-@app.get("/test_files/")
-async def test_files(files: List[bytes] = File(...)):
-    contents = """
-        <body>
-            <form action="/receive_audio_files/" enctype="multipart/form-data" method="get">
-                <input name="files" type="file" multiple>
-                <input type="submit">
-            </form>
-        </body>
-    """
-    return HTMLResponse(content=contents)
-  
-# 웹소켓 설정 ws://127.0.0.1:8000/ws 로 접속할 수 있음
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    print(f"client connected : {websocket.client}")
-    await websocket.accept() # client의 websocket접속 허용
-    await websocket.send_text(f"Welcome client : {websocket.client}")
-    all_sockets.append(websocket)
-
-    while True:
-        data = await websocket.receive_json()  # client 메시지 수신대기
-        print(f"message received : {data} from : {websocket.client}")
-
-        if data["type"] == "init":
-            init(data["device_name"], websocket)
-        elif data["type"] == "start":
-            await start_chirps()
-        else:
-            print("정의되지 않은 type입니다.")
-    return
     
 
 # 개발/디버깅용으로 사용할 앱 구동 함수
